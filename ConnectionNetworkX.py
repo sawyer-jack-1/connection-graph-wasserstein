fcd7f00550575c91008f855b879c6e44075d4c75
sawyerjack <s5robert@ucsd.edu>
2023-04-06 13:53:47 -0700

    Improving sparse CNX and puppet post

--- ConnectionNetworkX.py
+++ ConnectionNetworkX.py
@@ -9,9 +9,9 @@ class ConnectionNetworkX(nx.Graph):
         self.dimConnection = dim
         self.nNodes = self.number_of_nodes()
         self.nEdges = self.number_of_edges()
-        self.connectionIncidenceMatrix = scipy.sparse.csr_matrix((self.nNodes * self.dimConnection, self.nEdges *
+        self.connectionIncidenceMatrix = scipy.sparse.lil_matrix((self.nNodes * self.dimConnection, self.nEdges *
                                                                   self.dimConnection), dtype=float)
-        self.connectionLaplacianMatrix = scipy.sparse.csr_matrix((self.nNodes * self.dimConnection, self.nNodes *
+        self.connectionLaplacianMatrix = scipy.sparse.lil_matrix((self.nNodes * self.dimConnection, self.nNodes *
                                                                   self.dimConnection), dtype=float)
 
         self.initializeConenctionLaplacian()
@@ -21,25 +21,23 @@ class ConnectionNetworkX(nx.Graph):
             fromNode = e[0]
             toNode = e[1]
 
-            # colIndexRange = range(edgeIndex * self.dimConnection, (edgeIndex + 1) * self.dimConnection)
-            # rowIndexRangeFromNode = range(fromNode * self.dimConnection, (fromNode + 1) * self.dimConnection)
-            # rowIndexRangeToNode = range(toNode * self.dimConnection, (toNode + 1) * self.dimConnection)
+            colIndexRange = range(edgeIndex * self.dimConnection, (edgeIndex + 1) * self.dimConnection)
+            rowIndexRangeFromNode = range(fromNode * self.dimConnection, (fromNode + 1) * self.dimConnection)
+            rowIndexRangeToNode = range(toNode * self.dimConnection, (toNode + 1) * self.dimConnection)
 
-            for i in range(self.dimConnection):
-                self.connectionIncidenceMatrix[fromNode * self.dimConnection + i, edgeIndex * self.dimConnection + i] = 1
-                self.connectionIncidenceMatrix[toNode * self.dimConnection + i, edgeIndex * self.dimConnection + i] = -1
+            self.connectionIncidenceMatrix[rowIndexRangeFromNode, colIndexRange] = 1
+            self.connectionIncidenceMatrix[rowIndexRangeToNode, colIndexRange] = -1
 
         self.connectionLaplacianMatrix = self.connectionIncidenceMatrix * self.connectionIncidenceMatrix.transpose()
 
-        # print(self.connectionLaplacianMatrix.toarray())
     def updateEdgeSignature(self, edge, O):
 
         toNode = edge[1]
         edgeIndex = list(self.edges()).index(edge)
 
+
         for i in range(self.dimConnection):
             for j in range(self.dimConnection):
-                print(toNode * self.dimConnection + i, edgeIndex * self.dimConnection + j)
                 self.connectionIncidenceMatrix[toNode * self.dimConnection + i, edgeIndex * self.dimConnection + j] = (-1) * O[j][i]
 
         self.connectionLaplacianMatrix = self.connectionIncidenceMatrix * self.connectionIncidenceMatrix.transpose()
